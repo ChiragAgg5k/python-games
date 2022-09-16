b622fdd889b239d059b38039ba6ee2387e148c0c
ChiragAgg5k <chiragaggarwal5k@gmail.com>
2022-09-16 18:28:44 +0530

    Major changes

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,10 +1,10 @@
 import random
 
 
-class figure():
-    def __init__(self) -> None:
+class Figure():
+    def __init__(self, name: str) -> None:
 
-        self.base = """
+        self.base = f"""
         ---------------
         |      | 
         |      
@@ -12,9 +12,10 @@ class figure():
         |
         |
         ^^^^^^^^^^^^^^^
+\"{name} isn't present yet. Try to get all guesses correctly to not hurt {name} at all!\"
         """
 
-        self.hang1 = """
+        self.hang1 = f"""
         ---------------
         |      | 
         |      O
@@ -22,9 +23,10 @@ class figure():
         |
         |
         ^^^^^^^^^^^^^^^
+\"OOF! {name}'s head is here , that oughta hurt\"
         """
 
-        self.hang2 = """
+        self.hang2 = f"""
         ---------------
         |      | 
         |      O
@@ -32,47 +34,63 @@ class figure():
         |
         |
         ^^^^^^^^^^^^^^^
+\"If you bring your head closer to the screen , you can hear {name}'s screams\"
         """
 
-        self.hang3 = """
+        self.hang3 = f"""
         ---------------
         |      | 
         |      O
-        |      |-
+        |      |\\
         |
         |
         ^^^^^^^^^^^^^^^
+\"NOOOO! not {name}'s right hand , he needed it for a lot of stuff. lot of stuff.....\"
         """
 
-        self.hang4 = """
+        self.hang4 = f"""
         ---------------
         |      | 
         |      O
-        |     -|-
+        |     /|\\
         |
         |
         ^^^^^^^^^^^^^^^
+\"Welp , you can still try to save {name}'s legs.... walking is kind of a important thing you know\"
         """
 
-        self.hang5 = """
+        self.hang5 = f"""
         ---------------
         |      | 
         |      O
-        |     -|-
+        |     /|\\
         |     /
         |
         ^^^^^^^^^^^^^^^
+\"You might as well let {name} die at this point....\"
         """
 
-        self.hang6 = """
+        self.hang6 = f"""
         ---------------
         |      | 
         |      O
-        |     -|-
+        |     /|\\
         |     / \\
         |
         ^^^^^^^^^^^^^^^
+\"Remember, {name}'s blood is on YOUR HANDS!\"
         """
+        self.win = f"""
+        ---------------
+        |      
+        |
+        |      O
+        |     \\|/
+        |     / \\
+        ^^^^^^^^^^^^^^^
+\"{name} IS ALIVE , HOOOORAAAYYYYYYY!!!!!!!!!!!\"
+        """
+
         self.hanglist = [self.base, self.hang1, self.hang2,
                          self.hang3, self.hang4, self.hang5]
 
@@ -93,7 +111,7 @@ class figure():
         return self.hanglist[self.currenthang]
 
 
-class word():
+class Word():
 
     def __init__(self) -> None:
         self.word_list = ["absence", "abuse", "account", "accident", "beneath", "bend", "benefit", "biology", "bitter", "candidate", "campaign", "camera", "capacity", "capture", "deaf", "daughter", "deal", "decorate", "dialogue", "economy", "finance", "educate", "efficient",
@@ -113,39 +131,59 @@ class word():
         print("---------------\n")
 
 
-game_on = True
+if __name__ == "__main__":
+    game_on = True
 
-while game_on:
-    hangman = figure()
-    hangiter = iter(hangman)
+    hangname = input(
+        "\nLet's start by entering a name for our hangman , to add a bit of \"emotional value\" : ").capitalize()
 
-    wordz = word()
-    chosen = wordz.random_word()
+    while game_on:
+        hangman = Figure(hangname)
+        hangiter = iter(hangman)
 
-    blank = ["_"]*len(chosen)
+        wordz = Word()
+        chosen = wordz.random_word()
 
-    print(hangman.__next__())
+        blank = ["_"]*len(chosen)
+        correct_messages = iter(["First correct guess!\n", "Second correct guess! ,keep going man\n", "Third correct guess! , you are killing it man\n",
+                                 "Fourth correct guess! go go go\n", "Fift correct guess! , you are on fire\n", "Sixth correct guess! damn\n", "Seventh correct guess! when will this end?\n", "Eight correct guess! bruh how long is this word\n"])
 
-    while "".join(blank) != chosen:
+        print(hangman.__next__())
 
-        print("".join(blank))
+        while "".join(blank) != chosen:
 
-        player_guess = input("Player enter your guess : ")
+            print(
+                f"Number of letters left to guessed = { blank.count('_') }")
+            print("".join(blank))
 
-        if player_guess in chosen:
-            index_pos = [i for i, j in enumerate(chosen) if player_guess == j]
-            for i in index_pos:
-                blank[i] = list(chosen)[i]
+            player_guess = input("\nPlayer enter your guess : ")
 
-        else:
-            try:
-                print(hangiter.__next__())
-            except StopIteration:
-                print(hangman.hang6)
-                print("GAME OVER")
-                print("THE WORD WAS ", chosen)
-                break
+            if player_guess in chosen and player_guess != "":
+                index_pos = [i for i, j in enumerate(
+                    chosen) if player_guess == j]
+                for i in index_pos:
+                    blank[i] = list(chosen)[i]
 
-    cont = input("Do you want to play one more round? (y/n) : ")
-    if cont != "y":
-        break
+                print(next(correct_messages))
+
+            else:
+                try:
+                    print(hangiter.__next__())
+
+                except StopIteration:
+                    print(hangman.hang6)
+                    print("-----GAME OVER-----")
+                    print("The word was :", chosen)
+                    break
+
+        if "".join(blank) == chosen:
+
+            print(hangman.win)
+
+            print(
+                f"You got the correct answer {chosen} in {hangiter.currenthang-1} wrong guesse(s).")
+
+        cont = input("\nDo you want to play one more round? (y/n) : ")
+        if cont != "y":
+            break
+        print("\n"*100)
